a2b1689e313aff5755cc8e9f341ea841b0f1156b
Rohan Thapa <thaparohan2019@gmail.com>
2024-02-07 21:21:22 +0545

    User count on admin dashboar

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,17 +1,35 @@
-import "./dashboard.css"
+import { useEffect } from "react"
+import  "./dashboard.css"
+import { useState } from "react"
+import axios from "axios";
 const Dashboard = () => {
-
+    const [roleCount, setRoleCount] = useState();
+    useEffect(()=>{
+        const fetchRoleCount = async () =>{
+            await axios.get("http://localhost:8080/api/users/count",{
+                withCredentials: true,
+            }).then((response)=>{
+                // console.log
+                setRoleCount(response.data.data);
+            }).catch((error)=>{
+                console.log(error)
+            })
+        }
+        fetchRoleCount();
+    },[])
     return (
         <>
             <div className="countContainer">
-                <div className="studentCount">
-                    <h3>Number of Students</h3>
-                    <h1>150</h1>
-                </div>
-                <div className="teacherCount">
-                    <h3>Number of Teachers</h3>
-                    <h1>15</h1>
-                </div>
+                {
+                    roleCount?.map((role)=>{
+                        return <div className="studentCount">
+                        <h3>No of {role.role}</h3>
+                        <h1>{role.no_of_user}</h1>
+                    </div>
+                    })
+                }
+                
+                
             </div>
             <h1>Graph for student perfomance in exam overall coming soon ...</h1>
         </>
